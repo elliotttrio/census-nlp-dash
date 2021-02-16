34e30fd6e790eee0e7c9844ed89f8a9130dd5554
Elliott <eat2153@columbia.edu>
2021-02-16 16:41:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
-import torch
 from dash.dependencies import Input, Output, State
 from transformers import BartTokenizer, BartForConditionalGeneration
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
